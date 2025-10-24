19bcd0b6a03cba1ea47509fc76042aec86308a2a
camilo quiroga <camilo_quirogam@cun.edu.co>
2025-10-23 20:27:27 -0500

    Clase 23-10-2025 (Variables)

---
+++ variables.php
@@ -0,0 +1,24 @@
+<?php 
+// Variables de diferentes tipos 
+$nombre = "Juan"; 
+// String (texto) 
+$edad = 25; 
+// Integer (número entero) 
+$altura = 1.75; 
+// Float (decimal) 
+$esEstudiante = true; 
+// Boolean (verdadero/falso) 
+// Mostrar las variables 
+
+echo "Nombre: " . $nombre; 
+echo "<br>Edad: " . $edad; 
+echo "<br>Altura: " . $altura . " metros<br>"; 
+
+echo "<p style='color:orange; font-size:60px;'>
+La suma de edad y altura es: " . $edad + $altura . "</p>";
+
+echo "<h2 style='color:red; font-size:60px;'>" . $nombre. 
+" tiene <span style='color: blue;'>" .$edad. "</span> años</h2>";
+
+?>
+
